253f6ed75d3e727a6986f9b433828a203f1298cb
Arkadij Kuzhel <arkuzhel@gmail.com>
2013-10-07 17:29:07 +0300

    Create 2nd Unit test

--- tests/LogikaTest.php
+++ tests/LogikaTest.php
@@ -1,6 +1,6 @@
 <?php
 
-define('LOGIKA_PHPUNIT_TESTING', 'TRUE');
+define('LOGIKA_PHPUNIT_TESTING', 1);
 
 require_once '../Logika.php';
 
@@ -21,9 +21,32 @@ class LogikaTest extends PHPUnit_Framework_TestCase
 
     public function testInput()
     {
-        $instance = $this->_getInstance();
+        $instance = $this->_getInstance(2);
         $this->assertEquals('10', $instance->input('10'));
         $this->assertEquals('08', $instance->input('08'));
+        $this->assertEquals('10', $instance->input('103'));
+        $this->assertEquals('55', $instance->input('55'));
+    }
+
+    public function testCompare()
+    {
+        $instance = $this->_getInstance(4);
+        $secretNumber = $instance->test_getNumber();
+
+        $instance->input(strrev($secretNumber));
+        $this->assertFalse($instance->compare());
+
+        $instance->input($secretNumber);
+        $this->assertTrue($instance->compare());
+
+        $guessNumber = '6540';
+        $instance->input($guessNumber);
+        Debug::log(array($guessNumber, $secretNumber));
+        if ($guessNumber == $secretNumber) {
+            $this->assertTrue($instance->compare());
+        } else {
+            $this->assertFalse($instance->compare());
+        }
     }
 
 }
