5c03ab95239a7b526ef6127e4d4cd9c8c5dafb17
Arkadij Kuzhel <arkuzhel@gmail.com>
2013-10-09 09:32:00 +0300

    Improve test by using data provider

--- tests/AnalysisMatrixTest.php
+++ tests/AnalysisMatrixTest.php
@@ -19,24 +19,34 @@ class AnalysisMatrixTest extends PHPUnit_Framework_TestCase
         return $instance;
     }
 
-    public function testGuessLike_x_x()
+    public function dataGuessLike_x_x()
     {
-        $guess = '6540';
-        $assertRows = array(
-            'guessLike_0_0' => array('123789', '123789', '123789', '123789'),
-            'guessLike_4_x' => array('0456', '0456', '0456', '0456'),
-            'guessLike_4_0' => array('045', '046', '056', '456'),
+        return array(
+            array('guessLike_0_0', '6540', array('123789', '123789', '123789', '123789')),
+            array('guessLike_4_x', '6540', array('0456', '0456', '0456', '0456')),
+            array('guessLike_4_0', '6540', array('045', '046', '056', '456')),
         );
-        foreach ($assertRows as $method => $assertRow) {
-            $instance = $this->_getInstance(4, 10);
-            $instance->$method($guess);
-            $matrix = $instance->test_getMatrix();
-            Debug::log(array('--------------------------' => $method, '$matrix' => $matrix, '$assertRow' => $assertRow));
-            $this->assertCount(4, $matrix);
-            foreach ($matrix as $i => $row) {
-                Debug::log(array(' $i' => $i, '$row' => $row, '$assertRow  ' => $assertRow));
-                $this->assertEquals($assertRow[$i - 1], $row);
-            }
+    }
+
+    /**
+     * @dataProvider dataGuessLike_x_x
+     *
+     * @param string $method wich method to test
+     * @param int $y length of secret number
+     * @param string $guess guess number
+     * @param array $assertRow expected matrix state
+     * @param int $x secret number base
+     */
+    public function testGuessLike_x_x($method, $guess, array $assertRow, $x = 10)
+    {
+        $instance = $this->_getInstance(strlen($guess), $x);
+        $instance->$method($guess);
+        $matrix = $instance->test_getMatrix();
+        Debug::log(array('--------------------------' => $method, '$matrix' => $matrix, '$assertRow' => $assertRow));
+        $this->assertCount(4, $matrix);
+        foreach ($matrix as $i => $row) {
+            Debug::log(array(' $i' => $i, '$row' => $row, '$assertRow  ' => $assertRow));
+            $this->assertEquals($assertRow[$i - 1], $row);
         }
     }
 
