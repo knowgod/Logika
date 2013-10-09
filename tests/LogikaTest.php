b71e97253199af5210915b7259d2ea698baaff63
Arkadij Kuzhel <arkuzhel@gmail.com>
2013-10-09 15:03:52 +0300

    Improve test by using data provider

--- tests/LogikaTest.php
+++ tests/LogikaTest.php
@@ -23,41 +23,52 @@ class LogikaTest extends PHPUnit_Framework_TestCase
     public function dataInput()
     {
         return array(
-            array('10', '10'),
-            array('08', '08'),
-            array('10', '103'),
-            array('55', '55'),
+            array('10', '10', 2),
+            array('08', '08', 2),
+            array('10', '103', 2),
+            array('55', '55', 2),
+            array('159', '15964', 3),
+            array('006', '0064', 3),
         );
     }
 
     /**
      * @dataProvider dataInput
+     *
+     * @param string $expect
+     * @param string $param
+     * @param int $inputLength
      */
-    public function testInput($expect, $param, $inputLength = 2)
+    public function testInput($expect, $param, $inputLength)
     {
         $instance = $this->_getInstance($inputLength);
         $this->assertEquals($expect, $instance->input($param));
     }
 
-    public function testCompare()
+    public function dataCompare()
     {
         $instance = $this->_getInstance(4);
         $secretNumber = $instance->test_getNumber();
+        return array(
+            array($instance, 'False', strrev($secretNumber)),
+            array($instance, 'True', $secretNumber),
+            array($instance, (('6540' == $secretNumber) ? 'True' : 'False'), '6540'),
+            array($instance, (('3186' == $secretNumber) ? 'True' : 'False'), '3186456'),
+        );
+    }
 
-        $instance->input(strrev($secretNumber));
-        $this->assertFalse($instance->compare());
-
-        $instance->input($secretNumber);
-        $this->assertTrue($instance->compare());
-
-        $guessNumber = '6540';
+    /**
+     * @dataProvider dataCompare
+     *
+     * @param Logika $instance
+     * @param string $assertMethod
+     * @param string $guessNumber
+     */
+    public function testCompare($instance, $assertMethod, $guessNumber)
+    {
+        $method = 'assert' . $assertMethod;
         $instance->input($guessNumber);
-        Debug::log(array($guessNumber, $secretNumber));
-        if ($guessNumber == $secretNumber) {
-            $this->assertTrue($instance->compare());
-        } else {
-            $this->assertFalse($instance->compare());
-        }
+        $this->$method($instance->compare());
     }
 
 }
